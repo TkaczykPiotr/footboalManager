a0af2ad5d17467b5b2877857590792d35808b804
Skafii <piotrtkaczyk21a@wp.pl>
2022-05-31 19:56:28 +0200

    data player

---
+++ footboal-manager/src/data/PlayerData.js
@@ -0,0 +1,174 @@
+import React from 'react';
+
+export const PlayerData = [
+ {
+    id: 1,
+    idTeam: 1,
+    name: 'Neto',
+    cost: '100 000',
+    number: 13,
+    position: 'goalkeeper'
+  },
+  {
+    id: 2,
+    idTeam: 1,
+    name: 'ter Stegen',
+    cost: '100 000',
+    number: 1,
+    position: 'goalkeeper',
+    image: 'https://img.a.transfermarkt.technology/portrait/big/74857-1599987750.jpg?lm=1'
+  },
+  {
+    id: 3,
+    idTeam: 1,
+    name: 'Jordi Alba',
+    cost: '150 000',
+    number: 18,
+    position: 'left-back'
+  },
+  {
+    id: 4,
+    idTeam: 1,
+    name: 'Araujo',
+    cost: '550 000',
+    number: 4,
+    position: 'centre-back'
+  },
+  {
+   id: 5,
+   idTeam: 1,
+   name: 'Eric Garcia',
+   cost: '450 000',
+   number: 24,
+   position: 'centre-back'
+  },
+  {
+     id: 6,
+     idTeam: 1,
+     name: 'Pique',
+     cost: '450 000',
+     number: 3,
+     position: 'centre-back'
+  },
+  {
+   id: 7,
+   idTeam: 1,
+   name: 'Dest',
+   cost: '150 000',
+   number: 2,
+   position: 'right-back'
+   },
+  {
+   id: 8,
+   idTeam: 1,
+   name: 'Dani Alves',
+   cost: '250 000',
+   number: 8,
+   position: 'right-back'
+   },
+  {
+   id: 9,
+   idTeam: 1,
+   name: 'Busquets',
+   cost: '600 000',
+   number: 5,
+   position: 'defensive-midfield'
+  },
+  {
+  id: 10,
+  idTeam: 1,
+  name: 'Pedri',
+  cost: '1 600 000',
+  number: 16,
+  position: 'central-midfield'
+  },
+  {
+    id: 11,
+    idTeam: 1,
+    name: 'De Jong',
+    cost: '2 600 000',
+    number: 21,
+    position: 'central-midfield'
+  },
+  {
+    id: 12,
+    idTeam: 1,
+    name: 'Gavi',
+    cost: '950 000',
+    number: 30,
+    position: 'central-midfield'
+  },
+  {
+    id: 13,
+    idTeam: 1,
+    name: 'Nico',
+    cost: '800 000',
+    number: 14,
+    position: 'central-midfield'
+    },
+  {
+  id: 14,
+  idTeam: 1,
+  name: 'Roberto',
+  cost: '480 000',
+  number: 20,
+  position: 'central-midfield'
+ },
+ {
+   id: 15,
+   idTeam: 1,
+   name: 'Puig',
+   cost: '380 000',
+   number: 6,
+   position: 'central-midfield'
+  },
+  {
+    id: 16,
+    idTeam: 1,
+    name: 'Ansu Fati',
+    cost: '1 380 000',
+    number: 10,
+    position: 'left-winger'
+   },
+  {
+     id: 17,
+     idTeam: 1,
+     name: 'Ferran Torres',
+     cost: '1 350 000',
+     number: 19,
+     position: 'left-winger'
+    },
+  {
+   id: 18,
+   idTeam: 1,
+   name: 'Dembele',
+   cost: '1 150 000',
+   number: 7,
+   position: 'right-winger'
+  },
+ {
+  id: 19,
+  idTeam: 1,
+  name: 'Adama Traore',
+  cost: '850 000',
+  number: 11,
+  position: 'right-winger'
+  },
+  {
+   id: 20,
+   idTeam: 1,
+   name: 'Depay',
+   cost: '1 850 000',
+   number: 9,
+   position: 'central-forward'
+    },
+  {
+  id: 21,
+  idTeam: 1,
+  name: 'Aubameyang',
+  cost: '950 000',
+  number: 25,
+  position: 'central-forward'
+ }
+
+];
--- footboal-manager/src/data/TeamData.js
+++ footboal-manager/src/data/TeamData.js
@@ -6,21 +6,21 @@ export const TeamData = [
     name: 'FC Barcelona',
     city: 'Barcelona',
     budget: '10 000 000',
-    numberOfPlayer: 23
+    numberOfPlayer: 21
   },
   {
     id: 2,
     name: 'Real Madrid',
     city: 'Madrid',
     budget: '10 000 000',
-    numberOfPlayer: 21
+    numberOfPlayer: 20
   },
   {
     id: 3,
     name: 'Atletico Madrid',
     city: 'Madrid',
     budget: '10 000 000',
-    numberOfPlayer: 26
+    numberOfPlayer: 22
   }
 
 ];
